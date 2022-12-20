fa12931e798922af2f887f24993c396638d37171
fitz95 <fitzgeraldnsong@gmail.com>
2022-12-20 13:10:02 +0100

    added js file for mobile-menu with corresponding css and html

---
+++ myScript.js
@@ -0,0 +1,23 @@
+const hamburger = document.getElementById('hamburgericon');
+const closebutton = document.getElementById('close-icon');
+
+function openNav() {
+  document.getElementById('myNav').style.width = '100%';
+}
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
+}
+
+hamburger.addEventListener('click', openNav);
+closebutton.addEventListener('click', closeNav);
+const li= document.querySelectorAll(".links");
+const sec = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len= sec.length;
+    while(--len && window.scrollY + 97< sec[len].offsetTop){
+        
+    }
+}
+
+
