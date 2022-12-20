49016c91865437123adae7d2d64ea72689d95541
fitz95 <fitzgeraldnsong@gmail.com>
2022-12-20 14:42:51 +0100

    adding extra functionality to the desktop menu bar with corresponding html and css

--- myScript.js
+++ myScript.js
@@ -14,10 +14,13 @@ const li= document.querySelectorAll(".links");
 const sec = document.querySelectorAll('section');
 
 function activeMenu(){
-    let len= sec.length;
-    while(--len && window.scrollY + 97< sec[len].offsetTop){
-        
+    let len=sec.length;
+    while(--len && window.scrollY + 92 < sec[len].offsetTop){
+    li.forEach(liitem => liitem.classList.remove("active"));
+    li[len].classList.add("active");
     }
+    activeMenu();
+    window.addEventListener("scroll", activeMenu);
 }
 
 
